36e1b3d91d5cc333c5424006d339f20fc468c474
wioenena <wioenena@gmail.com>
2025-07-12 06:51:11 +0300

    Update build.zig

--- build.zig
+++ build.zig
@@ -5,8 +5,8 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("zigcord", .{
-        .root_source_file = b.path("src/lib.zig"),
+    const zigcord_mod = b.addModule("zigcord", .{
+        .root_source_file = b.path("./src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -17,17 +17,17 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    lib_mod.addImport("zigcord-gateway", gateway_mod);
+    zigcord_mod.addImport("zigcord-gateway", gateway_mod);
 
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "zigcord",
-        .root_module = lib_mod,
+        .root_module = zigcord_mod,
     });
 
     b.installArtifact(lib);
 
-    const lib_unit_tests = b.addTest(.{ .root_module = lib_mod });
+    const lib_unit_tests = b.addTest(.{ .root_module = zigcord_mod });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
