105ff4c7878d088e31d92a35d226a502f622394a
wioenena <wioenena@gmail.com>
2025-07-12 10:59:22 +0300

    Export StatusCode enum from gateway module

--- libs/gateway/lib.zig
+++ libs/gateway/lib.zig
@@ -1,3 +1,4 @@
 pub const constants = @import("constants.zig");
 pub const Event = @import("event.zig").Event;
 pub const OPCode = @import("opcode.zig").OPCode;
+pub const StatusCode = @import("status_code.zig").StatusCode;
