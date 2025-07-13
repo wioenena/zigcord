b7f9f450cf47332c42bccecb44544ef2357677d6
wioenena <wioenena@gmail.com>
2025-07-14 02:03:21 +0300

    Add gateway module Events and Structures namespaces

--- libs/gateway/lib.zig
+++ libs/gateway/lib.zig
@@ -2,3 +2,18 @@ pub const constants = @import("constants.zig");
 pub const Event = @import("event.zig").Event;
 pub const OPCode = @import("opcode.zig").OPCode;
 pub const StatusCode = @import("status_code.zig").StatusCode;
+
+pub const Events = struct {
+    pub const HeartbeatEvent = @import("events/send/heartbeat.zig");
+    pub const IdentifyEvent = @import("events/send/identify.zig");
+    pub const RequestGuildMembersEvent = @import("events/send/request_guild_members.zig");
+    pub const RequestSoundboardsSoundsEvent = @import("events/send/request_soundboard_sounds.zig");
+    pub const ResumeEvent = @import("events/send/resume.zig");
+    pub const UpdatePresenceEvent = @import("events/send/update_presence.zig");
+    pub const UpdateVoiceStateEvent = @import("events/send/update_voice_state.zig");
+};
+
+pub const Structures = struct {
+    pub const Activity = @import("structures/activity.zig").Activity;
+    pub const PresenceUpdate = @import("structures/presence_update.zig").PresenceUpdate;
+};
