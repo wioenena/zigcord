ecd6181f490f44d7f8257478cc5867eb10f1f21a
wioenena <wioenena@gmail.com>
2025-07-17 08:25:18 +0300

    Add WebSocket and logger modules as dependencies

--- build.zig
+++ build.zig
@@ -5,19 +5,42 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
+    const websocket = b.dependency("websocket", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const zigcord_mod = b.addModule("zigcord", .{
         .root_source_file = b.path("./src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const gateway_mod = b.addModule("zigcord-gateway", .{
+    const zigcord_gateway_mod = b.addModule("zigcord-gateway", .{
         .root_source_file = b.path("libs/gateway/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    zigcord_mod.addImport("zigcord-gateway", gateway_mod);
+    const zigcord_ws_mod = b.addModule("zigcord-ws", .{
+        .root_source_file = b.path("libs/ws/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const zigcord_logger_mod = b.addModule("zigcord-logger", .{
+        .root_source_file = b.path("libs/logger/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    zigcord_ws_mod.addImport("websocket", websocket.module("websocket"));
+    zigcord_ws_mod.addImport("zigcord-gateway", zigcord_gateway_mod);
+    zigcord_ws_mod.addImport("zigcord-logger", zigcord_logger_mod);
+
+    zigcord_mod.addImport("zigcord-gateway", zigcord_gateway_mod);
+    zigcord_mod.addImport("zigcord-ws", zigcord_ws_mod);
+    zigcord_mod.addImport("zigcord-logger", zigcord_logger_mod);
 
     const lib = b.addLibrary(.{
         .linkage = .static,
