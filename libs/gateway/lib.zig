42f2aa879315362e271163e263017e370d8baa62
wioenena <wioenena@gmail.com>
2025-07-17 08:24:43 +0300

    Rename gateway constants import to title case

--- libs/gateway/lib.zig
+++ libs/gateway/lib.zig
@@ -1,4 +1,4 @@
-pub const constants = @import("constants.zig");
+pub const Constants = @import("constants.zig");
 pub const Event = @import("event.zig").Event;
 pub const OPCode = @import("opcode.zig").OPCode;
 pub const StatusCode = @import("status_code.zig").StatusCode;
