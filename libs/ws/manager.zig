c296fab7b4e37676dcea116a83355b67015b42b4
wioenena <wioenena@gmail.com>
2025-07-22 09:30:38 +0300

    Add WebSocket connect method with basic shard support

--- libs/ws/manager.zig
+++ libs/ws/manager.zig
@@ -1,3 +1,7 @@
+const Shard = @import("shard.zig").Shard;
+const std = @import("std");
+const Logger = @import("zigcord-logger").Logger;
+
 pub const WebSocketManager = struct {
     pub const Options = struct {
         token: []const u8,
@@ -5,6 +9,8 @@ pub const WebSocketManager = struct {
         large_threshold: ?u8 = 250,
         // shard TODO: Implement this
         intents: u32,
+        allocator: std.mem.Allocator,
+        logger: Logger,
     };
 
     const Self = @This();
@@ -13,5 +19,11 @@ pub const WebSocketManager = struct {
 
     pub fn init(options: Options) Self {
         return .{ .options = options };
+    }
+
+    pub fn connect(self: Self) !void {
+        var shard = try Shard.init(self.options.allocator, self, self.options.logger);
+        defer shard.deinit();
+        try shard.connect();
     }
 };
